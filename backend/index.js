9223ab246bdf6f0269f5a50cc7e50699384a31a9
Rishwanth Perumandla <rishwanth100@gmail.com>
2023-10-22 12:21:10 -0400

    backend initiated

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const compression = require('compression')
+const cors = require('cors')
+const credentials = require('./middlewares/credentials');
+const corsOptions = require('./config/corsOptions');
+
+var cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+var apiResponse = require("./helpers/apiResponse");
+
+
+var indexRouter = require("./routes/index");
+
+
+const app = express()
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+app.use(compression())
+app.enable("trust proxy");
+app.use(cookieParser());
+app.use(express.json())
+
+// Index Router
+app.use("/", indexRouter)
+
+
+// throw 404 if URL not found
+app.all("*", function (req, res) {
+  return apiResponse.notFoundResponse(res, "Page not found");
+});
+app.use((err, req, res) => {
+  if (err.name == "UnauthorizedError") {
+    return apiResponse.unauthorizedResponse(res, err.message);
+  }
+});
+
+module.exports = app;
